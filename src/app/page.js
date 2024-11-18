222b9b39293f3b3ed09fbd22db482c1f5340c7b3
Sude Cabuk <cabuksudeeda@gmail.com>
2024-11-18 16:21:41 +0300

    i18next

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,56 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useState } from "react";
+import { Dropdown } from "primereact/dropdown";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: {
+    translation: {
+      welcome:
+        "Sample text in English is also used in software development for testing user interfaces. For example, when developing text-editing software or word processors, placeholder text helps in ensuring that the software correctly handles formatting, text wrapping, and more.",
+    },
+  },
+  tr: {
+    translation: {
+      welcome:
+        "İngilizce örnek metin, kullanıcı arayüzlerini test etmek için yazılım geliştirmede de kullanılır. Örneğin, metin düzenleme yazılımı veya kelime işlemciler geliştirirken, yer tutucu metin, yazılımın biçimlendirme, metin kaydırma ve daha fazlasını doğru şekilde işlemesini sağlamaya yardımcı olur.",
+    },
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+  });
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [selectedLanguages, setSelectedlanguages] = useState("en");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const languages = [
+    { label: "English", value: "en" },
+    { label: "Turkish", value: "tr" },
+  ];
+
+  const handleLanguageChange = (lang) => {
+    setSelectedlanguages(lang);
+    i18n.changeLanguage(lang);
+  };
+  return (
+    <div>
+      <div className="card flex justify-content-center">
+        <Dropdown
+          value={selectedLanguages}
+          onChange={(e) => handleLanguageChange(e.target.value)}
+          options={languages}
+          optionLabel="label"
+          placeholder="Select a Language"
+          className="w-full md:w-14rem"
+        />
+        <h2>{i18n.t("welcome")}</h2>
+      </div>
     </div>
   );
 }
